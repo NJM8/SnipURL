45610062df5a1ddb44895539bf69eab0a265d90d
Nate Mallison <healthynate@gmail.com>
2019-11-20 13:48:09 -0500

    Added basic layout with input and submit button, basic url fetching functionality.

--- snipurl/src/App.vue
+++ snipurl/src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>SnipURL</h1>
+    <p>A simple url shortener, enter any complete valid url below to recieve an easy to remember shortened version!</p>
+    <input type="text" v-model="originalUrl">
+    <button @click="submitUrl">Submit</button>
+    <p>Shortened URL: {{ this.shortUrl }}</p>
   </div>
 </template>
 
+<style src="@/assets/styles.css" lang="css"></style>
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from "axios";
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      originalUrl: "",
+      shortUrl: ""
+    }
+  },
+  methods: {
+    submitUrl() {
+      if (this.originalUrl.length === 0) {
+        alert("Please enter a url");
+      }
+
+      axios.get(`http://localhost:8000/new/${this.originalUrl}`).then(response => {
+        if (response.data.urlOriginal === "Invalid Url") {
+          alert("please enter a valid url. ex: https://www.google.com");
+        }
+        this.shortUrl = response.data.urlShortened;
+      })
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
